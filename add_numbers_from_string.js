d81dbd847efe2f9756831af46a8a8ee581259ec3
Gowtham <gowthamm.5127@gmail.com>
2023-04-04 23:04:34 +0530

    Update add_numbers_from_string.js
    Updated questions

--- add_numbers_from_string.js
+++ add_numbers_from_string.js
@@ -1,3 +1,7 @@
+// nput string =  " 19.2acb1999 buy$23.004yyu456e089rr7"
+// Get a sum of all whole numbers from the given string.
+// eg: [ '19', '1999', '23', '456', '089', '7' ] = 115687 (sum of all whole numbers)
+
 const str = "19.2acb1999 buy$23.004yyu456e089rr7";
 const numbers = str.match(/\d+(\.\.d+)?/g);
 let count = 0;
